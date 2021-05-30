2cd26ac69716499ed34818e641999022942accbb
sammiee5311 <56215891+sammiee5311@users.noreply.github.com>
2021-05-30 17:15:25 +0900

    async update

--- models/machine.js
+++ models/machine.js
@@ -3,15 +3,6 @@ const {PythonShell} = require('python-shell')
 const filePath = "./util/python";
 const fileName = "main.py"
 
-const fetchData = () =>{
-    const promise = new Promise((resolve, reject) =>{
-        setTimeout(() => {
-            resolve('Done!');
-        }, 1500);
-    });
-    return promise;
-};
-
 class pythonModule{
     constructor(){
         this.fileName = fileName;
@@ -32,28 +23,20 @@ class pythonModule{
         this.options.args.length = 0;
     }
 
-    dischargeMedicines(){
-        PythonShell.run(this.fileName, this.options, (err, results) => {
-            if(err) throw err;
-            console.log(results);
-            if(results.length >= 1 && results[results.length-1] === 'success') {
-                setTimeout(() => {
-                    console.log('Discharging medicines.');
-                    fetchData()
-                    .then(() =>{
-                        console.log('Discharging medicines..');
-                        return fetchData();
-                    })
-                    .then(() => {
-                        console.log('Discharging medicines...');
-                        return fetchData();
-                    })
-                    .then(() => {
-                        console.log('Done!');
-                    })
-                }, 2000);
-            } 
-            else console.log("Fail");
+    async dischargeMedicines(){
+        const { success, err = '', results } = await new Promise((resolve, reject) => {
+            PythonShell.run(this.fileName, this.options, (err, results) => {
+                if(err){
+                    reject({ success: false, err });
+                }
+                if(results.length >= 1 && results[results.length-1] === 'success') {
+                    resolve({ success: true, results });
+                } 
+                else if (results[results.length-1] === 'fail'){
+                    console.log("Fail");
+                    resolve({ success: true, results });
+                }
+            });
         });
     }
 }
